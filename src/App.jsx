ffc766a5f39233948f022681b831d7e5f1efd55a
Bhavik Barai <bhavik5631@gmail.com>
2022-09-26 01:37:42 +0200

    Counter Increase Decrease Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 import "./App.css";
 
+
+
 function App() {
+ const [counter,addCounter] = useState(0)
+  
+
+const onClickHandler = () => {
+  addCounter(counter + 1)
+}
   return (
-    <div className="container mt-3">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input
-          type="text"
-          class="form-control"
-          name="name"
-          id="name"
-          aria-describedby="nameHelp"
-          placeholder="Enter your name"
-        />
-        <small id="nameHelp" class="form-text text-muted">
-          You can enter your name
-        </small>
-      </div>
+    <div>
+    <br></br>
+    <h1>{counter}</h1>
+    <br></br>
+    <button type="button" onClick={onClickHandler} className="btn btn-secondary">Increase</button>
+    <br></br>
+    <button type="button" onClick={() => {addCounter(counter - 1)}} className="btn btn-secondary">Decrease</button>
+    <br></br>
+    <button type="button" onClick={() => {addCounter(0)}} className="btn btn-primary">Reset</button>
     </div>
   );
 }
